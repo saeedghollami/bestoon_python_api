76ef2dcd61e398306aa698dcaa6b4c7f06aa8b61
Saeed Gholami <saeed.ghollami@gmail.com>
2019-02-23 12:30:00 +0330

    create get_expences and get_incomes to get latest expences and incomes defualt is 10

--- bestoon_api.py
+++ bestoon_api.py
@@ -1,11 +1,13 @@
 import requests
+import json
 
 class Url:
     BASE_URL = 'http://bestoon.ir'
-    SUBMIT_EXPENSE = BASE_URL + '/submit/expense'
-    SUBMIT_INCOME = BASE_URL + '/submit/income'
-    STATS = BASE_URL + '/q/generalstat'
-    
+    SUBMIT_EXPENSE = BASE_URL + '/submit/expense/'
+    GET_EXPENSES = BASE_URL + '/q/expenses/'
+    SUBMIT_INCOME = BASE_URL + '/submit/income/'
+    GET_INCOMES = BASE_URL + '/q/incomes/'
+    STATS = BASE_URL + '/q/generalstat/'
     
 
 class Expense:
@@ -34,17 +36,26 @@ class Expense:
         data = {'token': self.token}
         response = requests.post(Url.STATS, data=data)
         if self._check_response(response):
-            expenses = response.json()['expense']
-            content = f'Total of Expenses: {expenses.get("amount__sum")}\n' \
-                      f'Number of Expenses: {expenses.get("amount__count")}'
+            content = response.json()['expense']
             return content
         else:
             return 'Connection Failed! '+ str(response.status_code)
-    
+        
+    def get_expenses(self, number=10):
+        data = {'token': self.token, 'number': number}
+        response = requests.post(Url.GET_EXPENSES, data=data)
+        if self._check_response(response):
+            content = json.loads(response.json())
+            return content
+        else:
+            return 'Problem' + str(response.status_code)
+
     
 class Income:
     def __init__(self, token):
         self.token = token
+    def _check_response(self, response):
+        return response.status_code == 200
         
     def add(self, **kwargs):
         kwargs['token'] = self.token  # add token to data
@@ -64,18 +75,26 @@ class Income:
         data = {'token': self.token}
         response = requests.post(Url.STATS, data=data)
         if self._check_response(response):
-            expenses = response.json()['income']
-            content = f'Total of Incomes: {expenses.get("amount__sum")}\n' \
-                      f'Number of Incomes: {expenses.get("amount__count")}'
+            content = response.json()['income']
             return content
         else:
             return 'Connection Failed! '+ str(response.status_code)
     
+    def get_incomes(self, number=10):
+        data = {'token': self.token, 'number': number}
+        response = requests.post(Url.GET_INCOMES, data=data)
+        if self._check_response(response):
+            content = json.loads(response.json())
+            return content
+        else:
+            return 'Problem' + str(response.status_code)
     
 if __name__ == '__main__':
     exp = Expense(token='DnjeBhgRBvjDk9YbnDxEwCZvN7GJUUgama3i5nna0VpABQX9')
-    inc= Expense(token='DnjeBhgRBvjDk9YbnDxEwCZvN7GJUUgama3i5nna0VpABQX9')
-    r = exp.stats()
-    s = inc.stats()
-    print(r)
-    print(s)
+    inc= Income(token='DnjeBhgRBvjDk9YbnDxEwCZvN7GJUUgama3i5nna0VpABQX9')
+    r = exp.get_expenses()
+    s = exp.stats()
+    x = inc.get_incomes(number=5)
+    #print(x)
+    #for item in x:
+        #print(item['fields']['date'], item['fields']['text'], item['fields']['amount'])
